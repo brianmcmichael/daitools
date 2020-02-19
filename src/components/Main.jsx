7a640420e8c5d1d84103fee2e4ca794bcf4e3da4
Brian McMichael <brian@brianmcmichael.com>
2020-02-19 17:08:41 -0500

    rm material ui

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,27 +1,11 @@
 import React from 'react';
-//import { makeStyles } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Container from '@material-ui/core/Container'
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
+import RateCalculator from './RateCalculator';
 
 
 const Main = (props) => {
   return (
     <React.Fragment>
-      <AppBar position="static">
-        <Toolbar>
-          <Typography variant="h6">
-            Daitools
-          </Typography>
-        </Toolbar>
-      </AppBar>
-      <Container>
-        Content here
-      </Container>
+        <RateCalculator />
     </React.Fragment>
   )
 }
---
+++ src/components/RateCalculator.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const RateCalculator = (props) => {
+
+  return (
+    <div>
+      Rates Calculator
+    </div>
+  )
+}
+
+export default RateCalculator
