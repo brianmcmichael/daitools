5913d6739b32658d8de5e53c174b932442dadddd
Brian McMichael <brian@brianmcmichael.com>
2020-02-19 23:53:06 -0500

    Updates

---
+++ precache-manifest.a1b04e2bb9557d359965cbb7e55c8048.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1b92bf5f63153a9e89c0ce7c3629ae1",
+    "url": "/daitools/index.html"
+  },
+  {
+    "revision": "29a05217e472d90aeaaa",
+    "url": "/daitools/static/css/2.68b8c555.chunk.css"
+  },
+  {
+    "revision": "076300ac68fcc5b3e0d5",
+    "url": "/daitools/static/css/main.6c64c297.chunk.css"
+  },
+  {
+    "revision": "29a05217e472d90aeaaa",
+    "url": "/daitools/static/js/2.6bccc2e4.chunk.js"
+  },
+  {
+    "revision": "2b88dbca0ea4f6599e46cb7fa0eec1e2",
+    "url": "/daitools/static/js/2.6bccc2e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "076300ac68fcc5b3e0d5",
+    "url": "/daitools/static/js/main.3cb4379f.chunk.js"
+  },
+  {
+    "revision": "9b79c5b9000b4b3565ac",
+    "url": "/daitools/static/js/runtime-main.4d932465.js"
+  },
+  {
+    "revision": "76a77e86600707cd03feb816c71d3e04",
+    "url": "/daitools/static/media/Dai126x126.76a77e86.png"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/daitools/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/daitools/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/daitools/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/daitools/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/daitools/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/daitools/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/daitools/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/daitools/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/daitools/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/daitools/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/daitools/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/daitools/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/daitools/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/daitools/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/daitools/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/daitools/static/media/outline-icons.ef60a4f6.woff"
+  }
+]);
